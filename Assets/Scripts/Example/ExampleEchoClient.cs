f7a28b1bf956304b730df5f6c7990ab8641775e7
Johannes Deml <public@deml.io>
2020-05-14 16:10:53 +0200

    Add input fields for server ip and port to test different setups

--- Assets/Scripts/Example/ExampleEchoClient.cs
+++ Assets/Scripts/Example/ExampleEchoClient.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using NetCoreServer;
 using UnityEngine;
@@ -44,11 +45,28 @@ namespace Supyrb
 		[SerializeField]
 		private float reconnectionDelay = 1.0f;
 
+		[Header("Settings Input")]
 		[SerializeField]
-		private InputField inputField = null;
+		private InputField serverIpInput = null;
 
 		[SerializeField]
-		private Button button = null;
+		private InputField serverPortInput = null;
+
+		[SerializeField]
+		private Button tcpConnectButton = null;
+
+		[SerializeField]
+		private Button udpConnectButton = null;
+
+		[SerializeField]
+		private Button disconnectButton = null;
+
+		[Header("Connection")]
+		[SerializeField]
+		private InputField messageInputField = null;
+
+		[SerializeField]
+		private Button sendMessageButton = null;
 
 		[SerializeField]
 		private Text serverResponseText = null;
@@ -58,9 +76,32 @@ namespace Supyrb
 
 		private IUnitySocketClient socketClient;
 		private byte[] buffer;
+		private bool disconnecting;
+		private bool applicationQuitting;
 
 		private void Start()
 		{
+			disconnecting = false;
+			tcpConnectButton.onClick.AddListener(TriggerTcpConnect);
+			udpConnectButton.onClick.AddListener(TriggerUdpConnect);
+			disconnectButton.onClick.AddListener(TriggerDisconnect);
+			sendMessageButton.onClick.AddListener(OnSendEcho);
+		}
+
+		private void OnDestroy()
+		{
+			Disconnect();
+		}
+
+		[ContextMenu("Connect")]
+		private void Connect()
+		{
+			if (socketClient != null && (socketClient.IsConnected || socketClient.IsConnecting))
+			{
+				Disconnect();
+				socketClient = null;
+			}
+
 			switch (type)
 			{
 				case Type.UDP:
@@ -74,34 +115,43 @@ namespace Supyrb
 			}
 
 			buffer = new byte[socketClient.OptionReceiveBufferSize];
-
+			
 			socketClient.OnConnectedEvent += OnConnected;
 			socketClient.OnDisconnectedEvent += OnDisconnected;
 			socketClient.OnErrorEvent += OnError;
 			socketClient.ConnectAsync();
-			button.onClick.AddListener(OnSendEcho);
 		}
 
-		private void OnDestroy()
+		[ContextMenu("Disconnect")]
+		private void Disconnect()
 		{
+			disconnecting = true;
+			socketClient.Disconnect();
+
+			while (socketClient.IsConnected)
+			{
+				Thread.Yield();
+			}
+
 			socketClient.OnConnectedEvent -= OnConnected;
 			socketClient.OnDisconnectedEvent -= OnDisconnected;
 			socketClient.OnErrorEvent -= OnError;
-
-			reconnect = false;
-			socketClient.Disconnect();
+			disconnecting = false;
 		}
 
 		private void Update()
 		{
 			UpdateStateInfoText();
-			if (!socketClient.IsConnected)
+			bool connected = socketClient != null && socketClient.IsConnected;
+			sendMessageButton.interactable = connected;
+			disconnectButton.interactable = connected;
+			tcpConnectButton.interactable = !connected;
+			udpConnectButton.interactable = !connected;
+
+			if (!connected)
 			{
-				button.interactable = false;
 				return;
 			}
-
-			button.interactable = true;
 
 			if (!socketClient.HasEnqueuedPackages())
 			{
@@ -121,32 +171,31 @@ namespace Supyrb
 
 		private void UpdateStateInfoText()
 		{
+			if (socketClient == null)
+			{
+				stateInfoText.text = "Connect to either TCP or UDP by pressing the buttons";
+				return;
+			}
+
 			var text = $"Server ip: {socketClient.Endpoint.Address}, Server port: {socketClient.Endpoint.Port}\n" +
 						$"Connecting: {socketClient.IsConnecting}, IsConnected: {socketClient.IsConnected}\n ";
 			stateInfoText.text = text;
 		}
 
-		private void OnSendEcho()
-		{
-			var message = Encoding.UTF8.GetBytes(inputField.text);
-			socketClient.Send(message);
-
-			for (int i = 0; i < repeatMessage; i++)
-			{
-				socketClient.Send(message);
-			}
-		}
-
 		private void OnConnected()
 		{
-			Debug.Log($"TCP client connected a new session with Id {socketClient.Id}");
+			Debug.Log($"{socketClient.GetType()} connected a new session with Id {socketClient.Id}");
 		}
 
 		private void OnDisconnected()
 		{
-			Debug.Log($"TCP client disconnected a session with Id {socketClient.Id}");
+			Debug.Log($"{socketClient.GetType()} disconnected a session with Id {socketClient.Id}");
+			if (applicationQuitting)
+			{
+				return;
+			}
 
-			if (reconnect)
+			if (reconnect && !disconnecting)
 			{
 				ReconnectDelayedAsync();
 			}
@@ -167,7 +216,48 @@ namespace Supyrb
 
 		private void OnError(SocketError error)
 		{
-			Debug.LogError($"TCP client caught an error with code {error}");
+			Debug.LogError($"{socketClient.GetType()} caught an error with code {error}");
+		}
+
+		private void TriggerTcpConnect()
+		{
+			type = Type.TCP;
+			ApplyInputAndConnect();
+		}
+
+		private void TriggerUdpConnect()
+		{
+			type = Type.UDP;
+			ApplyInputAndConnect();
+		}
+
+		private void TriggerDisconnect()
+		{
+			Disconnect();
+		}
+
+		private void ApplyInputAndConnect()
+		{
+			serverIp = serverIpInput.text;
+			serverPort = int.Parse(serverPortInput.text);
+			Connect();
+		}
+
+		[ContextMenu("Send message")]
+		private void OnSendEcho()
+		{
+			var message = Encoding.UTF8.GetBytes(messageInputField.text);
+			socketClient.Send(message);
+
+			for (int i = 0; i < repeatMessage; i++)
+			{
+				socketClient.Send(message);
+			}
+		}
+
+		private void OnApplicationQuit()
+		{
+			applicationQuitting = true;
 		}
 	}
 }
